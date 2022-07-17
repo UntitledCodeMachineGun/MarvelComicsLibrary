0de38aa65d9ff915f4ce24517732b93f8048410b
Volodimir <dev.v.nazarenko@gmail.com>
2022-07-17 18:54:18 +0300

    Added connection to Marvel API, getting elements. Refactored"

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+import 'regenerator-runtime/runtime';
+
+import { API_URL, URL_COMICS } from './constants/api'
+import { getDataApi } from './utils/GetDataApi'
+
+(async () =>
+{
+  const data = await getDataApi.getData(API_URL + URL_COMICS);
+
+  console.log(data);
+})();
