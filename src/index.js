9a5f9f15af19820652dc77bc42cde92abfec780d
Volodymyr <60801418+UntitledCodeMachineGun@users.noreply.github.com>
2022-07-19 15:09:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import App from '/src/components/App';
-import Comics from '/src/components/Comics';
+import App from '/src/components/App/index.js';
+import Comics from '/src/components/Comics/index.js';
 
 (async () =>
 {
